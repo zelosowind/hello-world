d57981c92ea16ba9cc11a66a0930d7939c55b1b4
asherkim <asherkim@b2link.co.kr>
2020-12-21 21:00:15 +0900

    리액트 개발 환경 직접 구축하기

---
+++ simple1.js
@@ -0,0 +1,24 @@
+function LikeButton() {
+    const [liked, setLiked] = React.useState(false);
+    const text = liked ? '좋아요 취소' : '좋아요';
+    return React.createElement(
+        'button',
+        { onClick: () => setLiked(!liked)},
+        text,
+    );
+}
+const domContainer1 = document.getElementById('root');
+ReactDOM.render(React.createElement(
+    'div',
+    null,
+    React.createElement(LikeButton),
+    React.createElement(LikeButton),
+    React.createElement(LikeButton),
+), domContainer1);
+
+React.createElement(
+    'div',
+    null,
+    React.createElement('p', null, 'hello') ,
+    React.createElement('p', null, 'world'),
+);
